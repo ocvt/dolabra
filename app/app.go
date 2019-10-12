95c96c93ede7ef8e63c39ec12961f6088f5a6c4f
Paul Wako <paul@walko.org>
2019-10-12 14:03:10 -0400

    start on middleware

--- app/app.go
+++ app/app.go
@@ -1,57 +1,59 @@
 package app
 
 import (
-//  "fmt"
-  "log"
-  "net/http"
+	//  "fmt"
+	"log"
+	"net/http"
 
-  "github.com/go-chi/chi"
-  "github.com/go-chi/chi/middleware"
-//  _ "github.com/lib/pq"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	//  _ "github.com/lib/pq"
 
-  "gitlab.com/ocvt/api/app/handler"
-  "gitlab.com/ocvt/api/config"
+	"gitlab.com/ocvt/api/app/handler"
+	"gitlab.com/ocvt/api/config"
 )
 
 var r *chi.Mux
+
 //var db *sql.DB
 
 // Initialize with predefined configuration
 func Initialize(config *config.Config) {
-//  dbURI := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True",
-//    config.DB.Username,
-//    config.DB.Password,
-//    config.DB.Host,
-//    config.DB.Port,
-//    config.DB.Name,
-//    config.DB.Charset)
-//
-//  db, err := gorm.Open(config.DB.Dialect, dbURI)
-//  if err != nil {
-//    log.Fatal("err")
-//  }
-//
-//  db = model.DBMigrate(db)
-  r = chi.NewRouter()
-  setRouters()
+	//  dbURI := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True",
+	//    config.DB.Username,
+	//    config.DB.Password,
+	//    config.DB.Host,
+	//    config.DB.Port,
+	//    config.DB.Name,
+	//    config.DB.Charset)
+	//
+	//  db, err := gorm.Open(config.DB.Dialect, dbURI)
+	//  if err != nil {
+	//    log.Fatal("err")
+	//  }
+	//
+	//  db = model.DBMigrate(db)
+	r = chi.NewRouter()
+	setRouters()
 }
 
 // Set routes
 func setRouters() {
-  // Set middleware
-  r.Use(middleware.RequestID)
-  r.Use(middleware.RealIP)
-  r.Use(middleware.Logger)
-  r.Use(middleware.Recoverer)
-  r.Use(handler.ProcessClientAuth)
+	// Set middleware
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	// Process JWT is present
+	r.Use(handler.ProcessClientAuth)
 
-  r.Route("/myaccount", func(r chi.Router) {
-    //r.Get("/", handler.GetMyAccount)
-    r.Get("/summary", handler.GetMyAccountSummary)
-    //r.Post("/register", handler.PostMyAccountRegister)
-  })
+	r.Route("/myaccount", func(r chi.Router) {
+		//r.Get("/", handler.GetMyAccount)
+		r.Get("/summary", handler.GetMyAccountSummary)
+		//r.Post("/register", handler.PostMyAccountRegister)
+	})
 }
 
 func Run(host string) {
-  log.Fatal(http.ListenAndServe(host, r))
+	log.Fatal(http.ListenAndServe(host, r))
 }
--- app/handler/middleware.go
+++ app/handler/middleware.go
@@ -1,45 +1,20 @@
 package handler
 
 import (
-//  "encoding/json"
-  "net/http"
-
-//  "github.com/go-chi/chi"
+	"log"
+	//  "encoding/json"
+	"net/http"
+	"strings"
+	//  "github.com/go-chi/chi"
 )
 
-func recoverHandler(next http.Handler) http.Handler {
-  fn := func(w http.ResponseWriter, r *http.Request) {
-    defer func() {
-      if err := recover(); err != nil {
-        log.Printf("panic: %+v", err)
-        http.Error(w, http.StatusText(500), 500)
-      }
-    }()
-
-    next.ServeHTTP(w, r)
-  }
-
-  return http.HandlerFunc(fn)
-}
-
-func ProcessClientAuth(next http.Handler) http.Handler
-func GetMyAccountSummary(w http.ResponseWriter, r *http.Request) {
-  // get access token from header
-  // get provider from query param
-  uniqueID, err = getUniqueID(r)
-  if err != nil {
-    return err
-  }
-
-  userSummary, err = getUserSummary(uniqueID)
-  //err includes unregistered
-  if err != nil {
-    return err
-  }
-
-  return userSummary
-
-  w.Header().Set("Content-Type", "text")
-  w.WriteHeader(http.StatusOK)
-  w.Write([]byte("hello wOrld"))
+func ProcessClientAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		authHeaderList := strings.Split(authHeader, "Bearer ")
+		jwt := authHeaderList[1]
+		log.Printf(jwt)
+		log.Printf("middleware called!")
+		next.ServeHTTP(w, r)
+	})
 }
