ccc30096cb72739cf8ab3b7822ccdc382010a982
Paul Wako <paulsw.pw@gmail.com>
2020-07-06 22:42:59 -0400

    Use sqlite datetime, remove officers PATCH method

--- app/handler/webtools_officers.go
+++ app/handler/webtools_officers.go
@@ -3,8 +3,6 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/go-chi/chi"
 )
 
 type officerStruct struct {
@@ -104,51 +102,6 @@ func GetWebtoolsOfficers(w http.ResponseWriter, r *http.Request) {
 	respondJSON(w, http.StatusOK, map[string][]*officerStruct{"officers": officers})
 }
 
-func PatchWebtoolsOfficers(w http.ResponseWriter, r *http.Request) {
-	// Get officerId
-	officerId, ok := getURLIntParam(w, r, "memberId")
-	if !ok {
-		return
-	}
-	action := chi.URLParam(r, "action")
-
-	// Get request body
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-	var jsonData = map[string]string{}
-	err := decoder.Decode(&jsonData)
-	if err != nil {
-		respondError(w, http.StatusBadRequest, err.Error())
-		return
-	}
-	data, ok := jsonData["data"]
-	if !ok {
-		respondError(w, http.StatusBadRequest, "POST body does not contain \"data\" field.")
-		return
-	}
-
-	// Permissions
-	// TODO Don't allow officers with less privileges to modify officers with more privileges
-	if !dbEnsureOfficer(w, officerId) {
-		return
-	}
-	if action != "position" && action != "expireDatetime" && action != "security" {
-		respondError(w, http.StatusNotFound, "Invalid path")
-		return
-	}
-
-	stmt := `
-		UPDATE officer
-		SET ` + action + ` = ?
-		WHERE member_id = ?`
-	_, err = db.Exec(stmt, action, data, officerId) // sqlvet: ignore
-	if !checkError(w, err) {
-		return
-	}
-
-	respondJSON(w, http.StatusNoContent, nil)
-}
-
 func PostWebtoolsOfficers(w http.ResponseWriter, r *http.Request) {
 	// Get request body
 	decoder := json.NewDecoder(r.Body)
@@ -174,7 +127,7 @@ func PostWebtoolsOfficers(w http.ResponseWriter, r *http.Request) {
 			expire_datetime,
 			position,
 			security)
-		VALUES (?, datetime('now'), ?, ?, ?)`
+		VALUES (?, datetime('now'), datetime(?), ?, ?)`
 	_, err = db.Exec(
 		stmt,
 		newOfficer.MemberId,
