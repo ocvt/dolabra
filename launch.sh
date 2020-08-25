96d68bf394224c815f0e2613e236272f23577da6
Paul Wako <paulsw.pw@gmail.com>
2020-08-25 17:05:40 -0400

    chore: use plain ol' docker instead of docker-compose + makefile

---
+++ launch.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+
+up () {
+  docker run \
+    --name dolabra \
+    --detach \
+    --restart unless-stopped \
+    --env-file dolabra.env \
+    --volume $PWD/data:/go/src/app/data:rw \
+    --publish 3000:3000 \
+    ocvt/dolabra:latest
+}
+
+down () {
+  stop
+  docker rm -f dolabra || true
+  rm -f data/dolabra-sqlite.sqlite3
+}
+
+stop () {
+  docker stop dolabra || true
+}
+
+logs () {
+  docker logs -f dolabra
+}
+
+###
+
+build () {
+  format
+  static-check
+  docker build -t ocvt/dolabra:latest .
+}
+
+deps () {
+	go get github.com/houqp/sqlvet
+	go get honnef.co/go/tools/cmd/staticcheck
+}
+
+format () {
+  gofmt -w .
+}
+
+full-test () {
+  down
+  up
+  test
+  down
+}
+
+static-check () {
+  sqlvet .
+  staticcheck ./app ./app/handler ./utils
+}
+
+test () {
+  sleep 1
+  python3 tests/main.py
+}
+
+
+$@
